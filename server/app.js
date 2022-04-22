05391db93f37dd5aae70f71a5318272a7b72a082
jinwoo park <swat018@gmail.com>
2022-04-22 14:20:21 +0900

    feat : 판맨 제품 등록 Vue 컴포넌트 구성

--- server/app.js
+++ server/app.js
@@ -93,6 +93,19 @@ app.post('/upload/:productId/:type/:fileName', async (request, res) => {
     });
 });
 
+app.get('/download/:productId/:fileName', (request, res) => {
+    const {
+        productId,
+        type,
+        fileName
+    } = request.params;
+    const filepath = `${__dirname}/uploads/${productId}/${fileName}`;
+    res.header('Content-Type', `image/${fileName.substring(fileName.lastIndexOf("."))}`);
+    if (!fs.existsSync(filepath)) res.send(404, {
+        error: 'Can not found file.'
+    });
+    else fs.createReadStream(filepath).pipe(res);
+});
 
 app.post('/apirole/:alias', async (request, res) => {
     if(!request.session.email) {
